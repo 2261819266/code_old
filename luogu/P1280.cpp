8afcce7ce678ddc0a630cae39c8260760a127979
yuxinhao <2261819266@qq.com>
2023-01-30 16:07:42 +0800

    P1280.cpp

---
+++ luogu/P1280.cpp
@@ -0,0 +1,27 @@
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+
+const int maxn = 1e4 + 8;
+std::vector<int> a[maxn];
+int f[maxn];
+
+int main() {
+    int n, m;
+    scanf("%d%d", &n, &m);
+    for (int i = 0; i < m; i++) {
+        int x, y;
+        scanf("%d%d", &x, &y);
+        a[x].push_back(y);
+    }
+
+    for (int i = n; i > 0; i--) {
+        if (a[i].empty()) f[i] = f[i + 1] + 1;
+        else {
+            for (auto j : a[i]) {
+                f[i] = std::max(f[i], f[i + j]);
+            }
+        }
+    }
+    printf("%d\n", f[1]);
+}
